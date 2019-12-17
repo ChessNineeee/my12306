6beaae72afb8687a25d5c7ec86d2e6162abd3e6e
ChessNineeee <709030194@qq.com>
2019-12-17 14:05:45 +0800

    First Commit

---
+++ app/src/main/java/com/example/experiment/ItemActivity.java
@@ -0,0 +1,57 @@
+package com.example.experiment;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class ItemActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.navi);
+        }
+        navigationView.setCheckedItem(R.id.navi_info);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    public void click(View v){
+        startActivity(new Intent(ItemActivity.this, MainActivity.class));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/experiment/LostFindActivity.java
@@ -0,0 +1,12 @@
+package com.example.experiment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class LostFindActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_find);
+    }
+}
